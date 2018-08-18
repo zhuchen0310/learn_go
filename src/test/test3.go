4e8d6738f811ee344b7a638c8aec8eb09ab721ef
zhuchen0310 <448290415@qq.com>
2018-08-18 23:59:16 +0800

    test_def

--- src/test/test3.go
+++ src/test/test3.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"math"
-	"time"
 	"fmt"
 )
 
@@ -73,6 +72,7 @@ func main() {
 
  */
 
+ /*
 func main() {
 	today := time.Now().Weekday()
 	println(time.Sunday)
@@ -86,4 +86,121 @@ func main() {
 	default:
 		fmt.Println("haha")
 	}
+}
+ */
+/*
+func main() {
+	var i, j int = 1, 12
+	p := &i
+	f := &i
+	println(f)
+	*f = 2
+	println(*f)
+	println(i, j, p, *p)
+
+
+}
+*/
+
+type adict struct {
+	key string
+	value int
+}
+
+func main()  {
+	/*
+	v := adict{"a",4}
+	p := &v
+	p.key = "ERE"
+	fmt.Println(v)
+	 */
+	// var (
+	// 	v1 = adict{}
+	// 	v2 = adict{"2", 5}
+	// 	v3 = adict{key: "rer"}
+	// 	p = &adict{"#@#", 3}
+	// )
+	//fmt.Println(v1, v2, v3, p)
+	/*
+
+	 var a [10] int
+	  s := a[7: ]
+	  b := a[6:]
+	  p := &a[8]
+	  *p = 3
+	  s[0] = 1
+	 fmt.Println(b)
+	 */
+
+	 /*
+
+	  a := []int{1, 2, 3}
+	  fmt.Println(a)
+	  b := []struct {
+		  k int
+		  v bool
+	  }{
+	  	{1, true},
+	  	{1, true},
+	  }
+	  fmt.Println(b)
+
+	  c := []int{}
+	  fmt.Println(c)
+	 	  */
+	 	  /*
+
+	var ere []int
+	er := []int{}
+	fmt.Println(er, ere)
+	fmt.Println(len(ere), cap(ere), ere)
+	if er != nil{
+		fmt.Println("%#H#I#")
+	}
+	 	   */
+
+	 	   /*
+
+	era := make([]int, 0, 10)
+	fmt.Println(era)
+	 	    */
+	 	    /*
+
+	 	    e := [][]int{[]int{1, 2}}
+	 	    fmt.Println(e)
+	 	    	 	     */
+
+
+	borad := [][] string{
+		[]string{"_", "_", "#"},
+		[]string{"_", "ew", "#"},
+	}
+
+	//fmt.Println(borad)
+
+	borad[0][0] = "T"
+		//fmt.Println(borad)
+
+	borad[1][2] = "Y"
+			//fmt.Println(borad)
+
+	//for i := 0; i < len(borad); i++{
+	//	fmt.Println(borad[i])
+	//}
+
+	for i := 0; i < 10; i++ {
+		//println(i)
+	}
+
+	var s []int
+	//s = append(s, 9)
+	//fmt.Println(s)
+	s = append(s, 6, 7, 8, 3)
+	//	fmt.Println(s)
+
+
+	for _, i := range s{
+		fmt.Printf("%d\n", i)
+	}
+
 }
---
+++ src/test/test_packet.go
@@ -0,0 +1,12 @@
+package test
+
+func Add(x, y int)  int{
+	return x + y
+}
+
+func Class(z string) string{
+	return z
+}
+
+
+
